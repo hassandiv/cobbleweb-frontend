7c5302ee10f71e762f465071da4c7b717238391f
hassan hafez <hassanhafez86@gmail.com>
2023-11-21 19:58:48 +0200

    header logo link to home

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -26,7 +26,10 @@ export default function Header() {
       suppressHydrationWarning={true}
     >
       <div className="container max-w-screen-xl flex flex-col items-start justify-between p-6 mx-auto md:flex-row">
-        <a className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
+        <a
+          href="/"
+          className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0"
+        >
           Cobbleweb
         </a>
         <nav className="flex flex-wrap items-center justify-center pl-6 ml-6 text-base border-l border-gray-200 md:mr-auto">
