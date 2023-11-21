b89cdac988b19df6ddd822323b79cf278e234428
hassan hafez <hassanhafez86@gmail.com>
2023-11-21 19:59:15 +0200

    footer logo link to home

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -20,7 +20,7 @@ export default function Footer() {
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <a href="#" className="flex items-center">
+            <a href="/" className="flex items-center">
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                 Cobbleweb
               </span>
